b75c6366d4851f4c6a165c74027861bfae1d65d0
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-02-24 20:57:20 +0500

    убрал импорт с ошибкой

--- src/database/schemas/__init__.py
+++ src/database/schemas/__init__.py
@@ -1,8 +1,6 @@
 __all__ = (
     "EventModel",
     "EventModelUpdate",
-    "VisitorModel",
 )
 
 from src.database.schemas.events_schemas import EventModel, EventModelUpdate
-from src.database.schemas.visitor_schema import VisitorModel
