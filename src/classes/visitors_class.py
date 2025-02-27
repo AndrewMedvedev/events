7868ca1cf1b9cc33e63484953ab5c328fd4b8a95
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-02-27 21:08:10 +0500

    Добавил креативности и теперь эндпоинт verify возвращает html страничку

--- src/classes/visitors_class.py
+++ src/classes/visitors_class.py
@@ -3,7 +3,7 @@ from io import BytesIO
 
 import pyqrcode
 from fastapi import status
-from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from starlette.responses import StreamingResponse
 
 from src.database import get_data
@@ -65,9 +65,37 @@ class Visitors:
             unique_string=unique_string,
         )
         if obj is not None:
-            return JSONResponse(content="Зарегестрирован")
+            html_content = """<!DOCTYPE html>
+            <html lang="ru">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Зарегистрирован</title>
+                <style> body { font-family: Arial, Helvetica, sans-serif; display: flex; justify-content: center; align-items: center; min-height: 100vh; background-color: #000000; margin: 0; } .message-container { background-color: #018d18; padding: 40px; border-radius: 10px; box-shadow: 0 10px 20px rgb(0, 0, 0); text-align: center; } h1 { font-size: 32px; color: #ffffff; margin-bottom: 20px; } p { font-size: 18px; color: #000000; } </style>
+            </head>
+            <body>
+                <div class="message-container">
+                    <h1>Зарегистрирован</h1>
+                </div>
+            </body>
+            </html>"""
+            return HTMLResponse(content=html_content)
         else:
-            return JSONResponse(content="Не зарегестрирован")
+            html_content = """<!DOCTYPE html>
+            <html lang="ru">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Не Зарегистрирован</title>
+                <style> body { font-family: Arial, Helvetica, sans-serif; display: flex; justify-content: center; align-items: center; min-height: 100vh; background-color: #000000; margin: 0; } .message-container { background-color: #b30606; padding: 40px; border-radius: 10px; box-shadow: 0 10px 20px rgb(0, 0, 0); text-align: center; } h1 { font-size: 32px; color: #ffffff; margin-bottom: 20px; } p { font-size: 18px; color: #000000; } </style>
+            </head>
+            <body>
+                <div class="message-container">
+                    <h1>Не Зарегистрирован</h1>
+                </div>
+            </body>
+            </html>"""
+            return HTMLResponse(content=html_content)
 
     async def make_qr(self):
         unique = await CRUD().get_visitor_unique_string(
