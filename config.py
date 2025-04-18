df5e625fd1d27f965713bbf8e5a1ba3b34c546f8
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-04-19 02:50:33 +0500

    исправляю ошибки в github action

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
-import os
-
-from dotenv import dotenv_values, find_dotenv, load_dotenv
+from dotenv import dotenv_values, find_dotenv
 
 config = dotenv_values(find_dotenv())
-
-if os.getenv("GITHUB_ACTIONS") == "true":
-    load_dotenv(".env.test")
-else:
-    load_dotenv()
 
 DB_HOST: str = config["DB_HOST"]
 DB_PORT: int = config["DB_PORT"]
