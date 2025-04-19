aa0ab5ba3bb206249be78d6ff5b83b7515b5de6c
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-04-19 17:08:38 +0500

    исправляю ошибки в github action # 13

--- config.py
+++ config.py
@@ -1,21 +1,26 @@
 from dotenv import dotenv_values, find_dotenv
 
-config = dotenv_values(find_dotenv())
+env_path = find_dotenv()
 
-DB_HOST: str = config["DB_HOST"]
-DB_PORT: int = config["DB_PORT"]
-DB_NAME: str = config["DB_NAME"]
-DB_USER: str = config["DB_USER"]
-DB_PASSWORD: str = config["DB_PASSWORD"]
 
-GET_DATA: str = config["GET_DATA"]
+config = dotenv_values(env_path)
 
-POSTGRES_HOST: str = config["POSTGRES_HOST"]
-POSTGRES_PORT: str = config["POSTGRES_PORT"]
-POSTGRES_PASSWORD: str = config["POSTGRES_PASSWORD"]
-POSTGRES_USER: str = config["POSTGRES_USER"]
-POSTGRES_DB: str = config["POSTGRES_DB"]
+
+class Settings:
+    DB_HOST: str = config["DB_HOST"]
+    DB_PORT: int = config["DB_PORT"]
+    DB_NAME: str = config["DB_NAME"]
+    DB_USER: str = config["DB_USER"]
+    DB_PASSWORD: str = config["DB_PASSWORD"]
+
+    GET_DATA: str = config["GET_DATA"]
+
+    POSTGRES_HOST: str = config["POSTGRES_HOST"]
+    POSTGRES_PORT: str = config["POSTGRES_PORT"]
+    POSTGRES_PASSWORD: str = config["POSTGRES_PASSWORD"]
+    POSTGRES_USER: str = config["POSTGRES_USER"]
+    POSTGRES_DB: str = config["POSTGRES_DB"]
 
 
 def get_db_url() -> str:
-    return f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    return f"postgresql+asyncpg://{Settings.DB_USER}:{Settings.DB_PASSWORD}@{Settings.DB_HOST}:{Settings.DB_PORT}/{Settings.DB_NAME}"
--- src/rest.py
+++ src/rest.py
@@ -1,12 +1,12 @@
 import aiohttp
 
-from config import GET_DATA
+from config import Settings
 
 from .utils import valid_answer
 
 
 async def get_user_data(user_id: int) -> dict:
     async with aiohttp.ClientSession() as session, session.get(
-        url=f"{GET_DATA}{user_id}", ssl=False
+        url=f"{Settings.GET_DATA}{user_id}", ssl=False
     ) as data:
         return await valid_answer(response=data)
