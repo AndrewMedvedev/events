af14564a2fa8798a174445baeba507382ff7917c
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-05-08 13:03:16 +0500

    изменил docker compose, добавил логику получения секретов

--- config.py
+++ config.py
@@ -3,7 +3,16 @@ from dotenv import dotenv_values, find_dotenv
 env_path = find_dotenv()
 
 
-config = dotenv_values(env_path)
+def load_config() -> dict:
+    env_path = find_dotenv(".env")
+
+    if not env_path:
+        env_path = find_dotenv(".test.env")
+
+    return dotenv_values(env_path)
+
+
+config = load_config()
 
 
 class Settings:
