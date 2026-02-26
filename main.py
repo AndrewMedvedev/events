aded0dfccf95c0ee928c39b6369b41b6ca47c996
andrey baidarka <andrejmedvedev2713@gmail.com>
2026-02-26 14:23:09 +0500

    26.02.2026 Проверил работоспособность сервиса

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import uvicorn
+
 from src.api.app import create_fastapi_app, setup_errors_handlers, setup_middleware
 
 app = create_fastapi_app()
 setup_middleware(app)
 setup_errors_handlers(app)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)  # noqa: S104
